bb153a0f2102ef694b0363ac71fcae2097404e19
alexckua <alex@net.ck.ua>
2017-04-15 23:24:29 +0300

    Added rspec for accounts controller

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
   factory :account do
-    provider 'MyString'
-    uid 'MyString'
-    token_twitter 'MyString'
-    secret_twitter 'MyString'
-    user nil
+    provider 'twitter'
+    uid '718397310183555072-1'
+    email 'test@gmail.com'
+    token_tw '718397310183555072-TyWnCq6jvwihuL4itjOVikL4oTiKS1s'
+    secret_tw '0XEJp1FD0t226gtEYJP6qLjvD4KFsc6gGx65M8poM7M0h'
+    user_id 1
   end
 end
