06be0b76793c61662fc7ef885e192c9b2443ea3d
Alex Andreiev <alex@net.ck.ua>
2018-11-13 02:18:41 +0200

    Add bootsrao devise views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def show
     render template: "pages/#{params[:page]}"
   rescue StandardError
